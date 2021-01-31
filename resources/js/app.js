0794d1f19c9acebd5480bb28a211a3a924347cb3
Sander Menke <info@sandermenke.nl>
2021-01-31 11:55:43 +0100

    Setup Vue and Bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
+import App from "./components/App"
+import Vue from 'vue'
+
 require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        aAaaaaa
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+    }
+</script>
