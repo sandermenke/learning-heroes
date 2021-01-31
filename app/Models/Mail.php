7476d60088225e1bea761148ac1baefde8d782f9
Sander Menke <info@sandermenke.nl>
2021-01-31 10:00:32 +0100

    Setup Mail model and migration

---
+++ app/Models/Mail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mail extends Model
+{
+    use HasFactory;
+
+    const STATUS_FAILED = 'failed';
+    const STATUS_SUCCESS = 'success';
+    const STATUS_QUEUED = 'queued';
+
+    const SERVICE_MAILJET = 'mailjet';
+    const SERVICE_SENDGRID = 'sendgrid';
+
+    const STATUSES = [
+      self::STATUS_FAILED,
+      self::STATUS_SUCCESS,
+      self::STATUS_QUEUED
+    ];
+
+    const SERVICES = [
+        self::SERVICE_MAILJET,
+        self::SERVICE_SENDGRID,
+    ];
+}
---
+++ database/migrations/2021_01_31_084845_create_mails_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Mail;
+
+class CreateMailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->id();
+            $table->string('mailing_service');
+            $table->string('message_id');
+            $table->enum('status', Mail::STATUSES);
+            $table->string('to');
+            $table->longText('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mails');
+    }
+}
