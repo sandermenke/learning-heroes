3459e4f947a865889b23e1a98c7968f432a4cd05
Sander Menke <info@sandermenke.nl>
2021-01-31 13:00:05 +0100

    Add form to send email from the frontend

--- resources/js/components/SendMail.vue
+++ resources/js/components/SendMail.vue
@@ -2,6 +2,21 @@
     <div class="card col-md-12">
         <div class="card-body">
             <h1 class="card-title">Send Mail</h1>
+            <form class="col-md-6">
+                <div class="mb-3">
+                    <label for="to" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="to" v-model="to" required>
+                </div>
+                <div class="mb-3">
+                    <label for="subject" class="form-label">Subject</label>
+                    <input type="text" class="form-control" id="subject" v-model="subject" required>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" id="message" rows="10" v-model="message" required></textarea>
+                </div>
+                <a @click="sendEmail" class="btn btn-primary">Submit</a>
+            </form>
         </div>
     </div>
 </template>
@@ -9,5 +24,19 @@
 <script>
     export default {
         name: 'send-mail',
+        data: () => ({
+            to: '',
+            subject: '',
+            message: '',
+        }),
+        methods: {
+            sendEmail() {
+                axios.post('/api/mail', {
+                    to: this.to,
+                    subject: this.subject,
+                    message: this.message,
+                }).then(() => this.$router.push({ name: 'home' }))
+            },
+        }
     }
 </script>
